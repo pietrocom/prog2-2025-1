f5cf1e17074361c5758eba1dea62964aeb81e9d3
pietrocom <pietro.comin@ufpr.br>
2025-04-16 18:30:16 -0300

    Estruturacao vina.h #1

--- A1/vina.h
+++ A1/vina.h
@@ -5,10 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+// ---- Defines ----
+
+#define NOME_MAX 100
+#define DATA_MAX 20
+
+
 // ---- Structs ----
+
+struct arquivo {
+    char nome[NOME_MAX];    
+    int uid;       
+    int tam_or;          
+    int tam_comp;         
+    char data_mod[DATA_MAX]; 
+    int ordem;              
+    int offset;              
+};
 
 
 // ---- Funcoes ----
+
 
 
 #endif
